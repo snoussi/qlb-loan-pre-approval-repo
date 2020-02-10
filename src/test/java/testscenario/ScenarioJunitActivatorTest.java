eefb8cd6d80418e6fd6b9b6a6fc09edcc7665d4f
Donato Marrazzo <donato.marrazzo@gmail.com>
2020-02-10 23:31:47 +0100

    fixed test scenario dependencies

--- src/test/java/testscenario/ScenarioJunitActivatorTest.java
+++ src/test/java/testscenario/ScenarioJunitActivatorTest.java
@@ -2,6 +2,6 @@ package testscenario;
 /**
 * Do not remove this file
 */
-@org.junit.runner.RunWith(org.drools.workbench.screens.scenariosimulation.backend.server.runner.ScenarioJunitActivator.class)
+@org.junit.runner.RunWith(org.drools.scenariosimulation.backend.runner.ScenarioJunitActivator.class)
 public class ScenarioJunitActivatorTest {
 }
